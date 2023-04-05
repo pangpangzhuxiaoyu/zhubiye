27d6211accad7b9a56548a8b42d0fbc1352fda03
hsh <3145469704@qq.com>
2023-04-05 23:44:47 +0800

    ad

--- computer graduation/StudentSystem/src/main/java/com/zhu/controller/Result.java
+++ computer graduation/StudentSystem/src/main/java/com/zhu/controller/Result.java
@@ -1,19 +1,25 @@
 package com.zhu.controller;
 
+/**
+ * 前后端返回结果类
+ */
 public class Result {
+    //存放后端返回的数据
     private Object data;
+    //存放正确/错误代码
     private Integer code;
+    //存放错误信息
     private String msg;
 
     public Result() {
     }
 
-    public Result(Object data, Integer code) {
+    public Result( Integer code,Object data) {
         this.data = data;
         this.code = code;
     }
 
-    public Result(Object data, Integer code, String msg) {
+    public Result(Integer code, String msg,Object data ) {
         this.data = data;
         this.code = code;
         this.msg = msg;
