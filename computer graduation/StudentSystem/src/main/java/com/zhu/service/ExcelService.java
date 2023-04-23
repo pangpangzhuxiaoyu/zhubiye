c3ea65b54c51a2676efb7449747975f476af3a8b
hezhu <he.zhu@dxc.com>
2023-04-23 21:17:21 +0800

    完成

--- computer graduation/StudentSystem/src/main/java/com/zhu/service/ExcelService.java
+++ computer graduation/StudentSystem/src/main/java/com/zhu/service/ExcelService.java
@@ -2,9 +2,10 @@ package com.zhu.service;
 
 import com.zhu.domain.Student;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Transactional
 public interface ExcelService {
     public XSSFWorkbook selectAll();
     public XSSFWorkbook selectByIds(int[] studentIds);
