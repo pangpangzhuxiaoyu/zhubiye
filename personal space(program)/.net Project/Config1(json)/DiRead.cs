8a4f69932ec70b4d169ff6391b14147665fe4533
hezhu <he.zhu@dxc.com>
2023-06-12 18:55:09 +0800

    .net config

---
+++ personal space(program)/.net Project/Config1(json)/DiRead.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Config1_json_
+{
+    internal class DiRead
+    {
+        public readonly IOptionsSnapshot
+    }
+}
---
+++ personal space(program)/.net Project/Config1(json)/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Config1_json_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 传统读取方法
+            //ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            //configurationBuilder.AddJsonFile("config.json",optional:true,reloadOnChange:true);
+            //IConfigurationRoot configurationRoot = configurationBuilder.Build();
+            //string name = configurationRoot["name"];
+            //string hh = configurationRoot.GetSection("hh:cc").Value;
+            //Console.WriteLine($"{name},{hh}");
+            //Console.ReadLine();
+            #endregion
+
+
+            ServiceCollection services = new ServiceCollection();
+            services.AddOptions().Configure<Config>();
+
+
+
+
+
+
+        }
+
+
+    }
+
+    public class Config
+    {
+        private string name;
+
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public class haha
+        {
+            private string cc;
+
+            public string Cc { get => cc; set => cc = value; }
+        }
+
+
+    }
+}
+
